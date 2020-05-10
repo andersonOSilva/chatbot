fe00482d2a7bc4a80ce48a6a7b529acec412c724
kassiano <kassiano.resende@gmail.com>
2020-05-10 20:53:05 -0300

    update

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
+#import files
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
-from flask import Flask, request
-
-token = "EAATIxNuC12oBAIYeKVagprW3X8xPjywkW1gezbCuPJ4kTBSsEso9DGlrvStpvKzZAb79WYEg3PRH7UbEudbag9RLyZAHOr222gNymIwLdk1ZCjwNjqs0X4JHAr586t9ZAUTDipH7KQEQW2OgDXIRGA5K6WwnP98E6462MIFkJgZDZD"
-FB_VERIFY_TOKEN = "TESTE123"
 app = Flask(__name__)
 
 
-
-@app.route('/', methods=['GET', 'POST'])
-def webhook():
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.data.decode())
-            text = data['entry'][0]['messaging'][0]['message']['text']
-            sender = data['entry'][0]['messaging'][0]['sender']['id']
-            payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}}
-            r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
-        except Exception as e:
-            print(traceback.format_exc())
-    elif request.method == 'GET': # Para a verificação inicial
-        if request.args.get('hub.verify_token') == FB_VERIFY_TOKEN:
-            return request.args.get('hub.challenge')
-        return "Wrong Verify Token"
-    return "Nothing"
+bot = ChatBot("Candice")
+bot.set_trainer(ListTrainer)
+bot.set_trainer(ChatterBotCorpusTrainer)
+bot.train("chatterbot.corpus.portuguese")
 
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')    
+    return str(bot.get_response(userText)) 
+if __name__ == "__main__":    
+    app.run()
